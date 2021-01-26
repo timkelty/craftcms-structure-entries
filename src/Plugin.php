fd346c59b3c11e78a083cb3855808cc289bdc871
Tim Kelty <tim@fusionary.com>
2021-01-25 19:08:22 -0500

    remove comment

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,54 +8,19 @@ use yii\base\Event;
 
 class Plugin extends \craft\base\Plugin
 {
-    // /**
-    //  * @inheritdoc
-    //  */
-    // public $allowLimit = false;
-
-    // /**
-    //  * @inheritdoc
-    //  */
-    // public $allowMultipleSources = false;
-
-    // /**
-    //  * @var int|null Branch limit
-    //  */
-    // public $branchLimit;
-
-    // /**
-    //  * @inheritdoc
-    //  */
-    // protected $settingsTemplate = '_components/fieldtypes/Categories/settings';
-
-    // /**
-    //  * @inheritdoc
-    //  */
-    // protected $inputTemplate = '_components/fieldtypes/Categories/input';
-
-    // /**
-    //  * @inheritdoc
-    //  */
-    // protected $inputJsClass = 'Craft.CategorySelectInput';
-
-    // /**
-    //  * @inheritdoc
-    //  */
-    // protected $sortable = false;
-
     /**
      * @inheritdoc
      */
-    public function init()
-    {
-        parent::init();
+    // public function init()
+    // {
+    //     parent::init();
 
-        Event::on(
-            Fields::class,
-            Fields::EVENT_REGISTER_FIELD_TYPES,
-            function (RegisterComponentTypesEvent $event) {
-                $event->types[] = \timkelty\craftcms\structureentries\fields\StructureEntries::class;
-            }
-        );
-    }
+    //     Event::on(
+    //         Fields::class,
+    //         Fields::EVENT_REGISTER_FIELD_TYPES,
+    //         function (RegisterComponentTypesEvent $event) {
+    //             $event->types[] = \timkelty\craftcms\structureentries\fields\StructureEntries::class;
+    //         }
+    //     );
+    // }
 }
